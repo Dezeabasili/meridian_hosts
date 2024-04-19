7a4c39def22474fb41304c3216a4fded2a15a14f
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-04-19 13:35:12 -0500

    corrected the baseURL

--- hotel-booking-server/utils/corsOrigins.js
+++ hotel-booking-server/utils/corsOrigins.js
@@ -1,7 +1,9 @@
 const corsOrigins = [
     // "https://meridianhomes.onrender.com"
     "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://meridian-hosts.onrender.com/"
+
 ]
 
 module.exports = corsOrigins
