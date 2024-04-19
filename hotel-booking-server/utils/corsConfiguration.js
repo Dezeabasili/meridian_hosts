1c6d4aa3b21c22c4a100d8290f7498337487ea31
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-04-19 11:23:11 -0500

    corrected the baseURL

--- hotel-booking-server/utils/corsConfiguration.js
+++ hotel-booking-server/utils/corsConfiguration.js
@@ -2,7 +2,8 @@ const corsOrigins = require('./corsOrigins')
 
 const corsConfiguration = {
     origin: (origin, callback) => {
-        if (corsOrigins.indexOf(origin) !== -1 || !origin) {
+        // if (corsOrigins.indexOf(origin) !== -1 || !origin) {
+        if (corsOrigins.indexOf(origin) !== -1 || origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
